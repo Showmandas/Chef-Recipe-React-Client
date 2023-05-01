260953ffc1d9920a2a8377bd4e16d6442db0be06
Showman_Das <Showmandas3216@gmail.com>
2023-05-02 00:09:03 +0600

    one another section or carousel complete

--- src/components/HomePage/ChefCarousel/Carousel.jsx
+++ src/components/HomePage/ChefCarousel/Carousel.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import img_1 from '../../../assets/images/slideimg-1.jfif'
-import img_2 from '../../../assets/images/slideimg-2.jfif'
-import img_3 from '../../../assets/images/slideimg-3.jfif'
+import img_1 from '../../../assets/images/slideimg-1.avif'
+import img_2 from '../../../assets/images/slideimg-2.avif'
+import img_3 from '../../../assets/images/slideimg-3.avif'
 
 const Carousel = () => {
     return (
         <div className='container'>
             <div className="row">
-                <div className="col-lg-12">
-                <div id="carouselExampleIndicators" className="carousel slide w-100">
+                <h1 className='text-secondary'>See Foods</h1>
+                <hr className='w-25 text-warning border-5' />
+                <div className="col-lg-12 mb-5 mt-5">
+                <div id="carouselExampleIndicators" className="carousel slide  w-100">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -18,20 +20,20 @@ const Carousel = () => {
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={img_1} className="d-block w-100 img-fluid" alt="slideimage-1"/>
+      <img src={img_1} className="d-block w-100 img-fluid" id='slideimg' alt="slideimage-1"/>
     </div>
     <div className="carousel-item">
-      <img src={img_2} className="d-block w-100 img-fluid" alt="slideimage-2"/>
+      <img src={img_2} className="d-block w-100 img-fluid" id='slideimg' alt="slideimage-2"/>
     </div>
     <div className="carousel-item">
-      <img src={img_3} className="d-block w-100 img-fluid" alt="slideimage-3"/>
+      <img src={img_3} className="d-block w-100 img-fluid" id='slideimg' alt="slideimage-3"/>
     </div>
   </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <button className="carousel-control-prev bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <button className="carousel-control-next bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
