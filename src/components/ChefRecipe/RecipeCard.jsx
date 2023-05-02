fe0d808338c60bb26d476116171c983fa9fa246a
Showman_Das <Showmandas3216@gmail.com>
2023-05-02 23:55:12 +0600

    add a toast message in favourite button in recipe

--- src/components/ChefRecipe/RecipeCard.jsx
+++ src/components/ChefRecipe/RecipeCard.jsx
@@ -3,9 +3,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const RecipeCard = ({recipe}) => {
     const{ingredients,rating,recipeName,cookingMethod}=recipe;
+    const handleFav=()=>{
+        toast("Added to Favourite!");
+
+    }
     return (
         <div className='col-lg-4'>
         
@@ -17,6 +23,9 @@ const RecipeCard = ({recipe}) => {
             </p>
             <p className='fs-5'><span className='fw-bold fs-4'>Cooking Method :</span> {cookingMethod}</p>
             <p><span className='fw-bold fs-4'>Rating : </span>{rating}/5</p>
+            <button className='btn bg-warning-subtle' onClick={handleFav}>Add to favourite</button>
+            <ToastContainer />
+
         </div>
         </div>
     );
